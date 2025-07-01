ebfa7ea43c806c9473e9729e8b29751efbe33737
Utkarsh Trivedi <trivediutkarsh31@gmail.com>
2025-07-01 23:40:43 +0530

    background change

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import background from "../public/background.jpg"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -34,8 +34,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}
         style={{
-          backgroundImage:
-            "url('https://dnm.nflximg.net/api/v6/BvVbc2Wxr2w6QuoANoSpJKEIWjQ/AAAAQdHbjrX0uL1mH7uUW7VZ-p5GJOSNNRko_9A_12Mlv5wVYQIDy4nuupxLHxkDj9V0e1OLBs_s8ZlxFkyhPlK5XkrlTK0c7yP42LLinQdYhz8lqfgUcOH68JAbEcY45jG02gCI1NyKVf1lqxsQFmcNN5szbv8.jpg?r=aee')",
+          backgroundImage:`url('/background.jpg')`,
           backgroundSize: "cover",
           backgroundRepeat: "repeat-y",
           backgroundAttachment: "fixed"
